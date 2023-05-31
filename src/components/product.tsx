edd41c3ff7deed971241df7cecfdd140c7373fce
Стасік Петрмк <stasps210@gmail.com>
2023-06-01 01:13:57 +0300

    update and add bug

---
+++ src/components/product.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { IProduct } from "../modules";
+
+interface productProps {
+  product: IProduct
+}
+
+export function Product(props: IProduct) {
+  return (
+    <div>
+    </div>
+  )
+}
---
+++ src/data/products.ts
@@ -0,0 +1,12 @@
+import { IProduct } from "../modules";
+
+export const products: IProduct[] = [  
+  {
+    id: 148929424,
+    title: 'test product',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'not found',
+    category: 'electronic'
+  }
+]
