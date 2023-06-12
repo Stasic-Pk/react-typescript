9e354c15eb7c79e71bb7964599321663e1a07ac6
Стасік Петрмк <stasps210@gmail.com>
2023-06-12 06:27:36 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import {Product} from "./components/product"
 import { IProduct } from './modules';
 import { useProducts } from './hooks/products';
+import { Loader } from './components/loader';
+import { ErrorMessage } from './components/errorMessage';
+import { Modal } from './components/modal';
 
 function App() {
 
@@ -9,13 +12,12 @@ function App() {
 
   return (
     <div className='box'>
+      <Modal/>
       <div className="div">
-        {loading && <h1 className='load'>Loading!</h1>}
-        {error && <div className='error'>{error}!</div>}
+        {loading && <Loader/>}
+        {error && <ErrorMessage error={error}/>}
         { products.map(product => <Product product = {product} key={product.id} />)}
 
-        {/* <Product product={products[0]} />
-        <Product product={products[1]} /> */}
       </div>
     </div>
   );
---
+++ src/components/errorMessage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useProducts } from "../hooks/products";
+
+interface ErrorMessageProps {
+  error: string
+}
+
+export function ErrorMessage({ error }: ErrorMessageProps) {
+
+  // const { error } = useProducts()
+
+  return <div className='error'>{error}!</div>
+}
